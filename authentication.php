a92d66917144f38ac9ca48e54ea21157f0e8e9f4
josefernandobr <fernando.leocadio.uerj@gmail.com>
2023-03-22 19:22:27 -0300

    ajuste versão produção infinity free

--- authentication.php
+++ authentication.php
@@ -1,7 +1,6 @@
 <?php  
 
     session_start();
-    // $_SESSION['superhero'] = "batman";
 
     include('connection.php');  
 
--- connection.php
+++ connection.php
@@ -1,8 +1,14 @@
 <?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "idk";  
+    // $host = "localhost";  
+    // $user = "root";  
+    // $password = '';  
+    // $db_name = "idk";  
+
+    $host = "sql205.epizy.com";  
+    $user = "epiz_33854390";  
+    $password = 'UbsMstZh1H';  
+    $db_name = "epiz_33854390_idk";  
+    
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
